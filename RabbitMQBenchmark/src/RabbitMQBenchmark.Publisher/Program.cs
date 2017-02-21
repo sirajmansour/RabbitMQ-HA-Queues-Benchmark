6b3ba19a1243d57cb5c2a5a12cc17b87ab7f93c8
sirajmansour <workbox92@live.com>
2017-02-21 16:31:03 +1100

    Initial Commit

---
+++ RabbitMQBenchmark/src/RabbitMQBenchmark.Publisher/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace RabbitMQBenchmark.Publisher
+{
+    public class Program
+    {
+        const string exchange = "massive.ingestion";
+        public static void Main(string[] args)
+        {
+            //args = new string[] { "5672", "subtitles.german", "1000000" };   //5672 5674 5676   //"subtitles.*","metadata.*"
+
+            string port = args[0];
+            string topic = args[1];
+            int limit = int.Parse(args[2]);
+
+            var factory = new ConnectionFactory() { HostName = "localhost", Port = int.Parse(port) };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange, ExchangeType.Topic, durable: true);
+
+                var properties = channel.CreateBasicProperties();
+                properties.Persistent = true;
+                properties.DeliveryMode = 2;
+                                
+                
+
+                for (int i = 1; i <= limit; i++)
+                {
+                    string message = $"[Message {i}]";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: exchange,
+                                 routingKey: topic,
+                                 basicProperties: properties,
+                                 body: body);
+
+                    Console.WriteLine($"Sent {message} on topic [{topic}]");
+                }
+
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
---
+++ RabbitMQBenchmark/src/RabbitMQBenchmark.Subsciber/Program.cs
@@ -0,0 +1,58 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQBenchmark.Subscriber
+{
+    public class Program
+    {
+        const string exchange = "massive.ingestion";
+        public static void Main(string[] args)
+        {
+            //args = new string[] { "5672", "subtitles.*" };   //5672 5674 5676   //"subtitles.*","metadata.*"
+
+            string port = args[0]; 
+            string bindingKey = args[1]; 
+
+            string queueName = bindingKey.Split(new char[] { '.' })[0];
+
+            var factory = new ConnectionFactory() { HostName = "localhost" , Port = int.Parse(port) };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange, "topic", durable: true, autoDelete:false);
+
+                channel.QueueDeclare(queueName, durable : true , autoDelete : false, exclusive:false);
+
+                channel.QueueBind(queue: queueName,
+                                  exchange: exchange,
+                                  routingKey: bindingKey);
+
+                var consumer = new EventingBasicConsumer(channel);
+                int i = 1;
+
+                consumer.Received += (model, e) =>
+                {
+                    var body = e.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    var routingKey = e.RoutingKey;
+                    Console.WriteLine("Received [{0}] on [{1}]", message, routingKey);
+
+                    channel.BasicAck(e.DeliveryTag, multiple: false);
+                };
+
+                
+                channel.BasicConsume(queue: queueName,
+                                     noAck: false,
+                                     consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
